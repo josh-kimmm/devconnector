42567bae683e21600f6fa52fcec5b14a7e2922fd
Joshua Kim <josh_kimmm@yahoo.com>
2021-05-13 17:26:21 -0700

    almost done with server side logic

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const UsersRoutes = require('./routes/api/users');
+const AuthRoutes = require('./routes/api/auth')
+const ProfileRoutes = require('./routes/api/profile');
+const PostsRoutes = require('./routes/api/posts');
 
 const app = express();
 
+// COnnect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', UsersRoutes);
+app.use('/api/auth', AuthRoutes);
+app.use('/api/profile', ProfileRoutes);
+app.use('/api/posts', PostsRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
